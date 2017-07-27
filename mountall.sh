e93aa3c1f40423240b0521924a6373882f7fed8c
alex <me@alexdaniel.me>
2017-07-27 16:54:23 -0500

    remove content after acd crackdown

--- mountall.sh
+++
@@ -1,6 +0,0 @@
-acd_cli sync
-acd_cli mount /mnt/acd/
-
-# Duplicate the following two lines for each backup dataset
-cat /root/backup-scripts/enc-passwd | ENCFS6_CONFIG='/root/backup-scripts/encfs.xml' encfs -S --reverse /mnt/movies /mnt/.movies
-cat /root/backup-scripts/enc-passwd | ENCFS6_CONFIG='/root/backup-scripts/encfs.xml' encfs -S /mnt/acd/backups/.1 /mnt/acd-movies
